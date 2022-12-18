32ffe3cde471832eb45c1453343637325f5a1ede
Jeff <jcgilbert70@gmail.com>
2022-12-18 14:35:11 -0600

    set up to code all menue functions

--- server.js
+++ server.js
@@ -32,11 +32,11 @@ function mainMenue() {
                     "View all departments",
                     "View all employees by department",
 
-                    "Edit employee role",
+                    "Edit employee",
 
-                    "Add new department",
-                    "Add new role",
                     "Add new employee",
+                    "Add new role",
+                    "Add new department",
 
                     "Terminate employee",
                     "Delete role type",
@@ -46,38 +46,94 @@ function mainMenue() {
             },
         ])
 
-// choice selection run corresponding function
+        // choice selection run corresponding function
+        .then((answer) => {
+            if (answer.mainMenue === "View all employees") {
+                viewAllEmployees();
 
+            } else if (answer.mainMenue === "View all roles") {
+                viewAllRoles();
+
+            } else if (answer.mainMenue === "View all departments") {
+                viewAllDepartments();
+
+            } else if (answer.mainMenue === "View employees by department") {
+                viewEmployeesByDepartment();
+
+            } else if (answer.mainMenue === "Edit employee") {
+                editEmployee();
+
+            } else if (answer.mainMenue === "Add new employeet") {
+                addEmployee();
+
+            } else if (answer.mainMenue === "Add new role") {
+                addRole();
+
+            } else if (answer.mainMenue === "Add new department") {
+                addDepartment();
+
+            } else if (answer.mainMenue === "Terminate employee") {
+                terminateEmployee();
+
+            } else if (answer.mainMenue === "Delete role type") {
+                deleteRole();
+
+            } else if (answer.mainMenue === "Delete department type") {
+                deleteDepartment();
+
+            } else {
+                connection.end();
+            }
+        });
 
 
     // function to view employee
+function viewAllEmployees() {
 
+}
     // function to view job type
+function viewAllRoles() {
 
+}
     // function to view departments
+function viewAllDepartments() {
 
+}
     // function to view employees by department
+function viewEmployeesByDepartment() {
 
+}
 
     // function to edit an emplyee role
+function editEmployee() {
 
+}
 
     // function to add employee
+function addEmployee() {
 
+}
     // function to add job type
+function addRole() {
 
+}
     // function to add dept
+function addDepartment() {
 
+}
 
 
     // function to delete employee
+function terminateEmployee() {
 
+}
     // function to delete job type
-
+function deleteRole() {
+    
+}
     // function to delete dept
-
-
-
-    // function to exit
+function deleteDepartment() {
+    
+}
 
 }
